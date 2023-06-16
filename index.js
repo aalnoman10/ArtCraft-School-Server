c314e9e25b3a0385fa013e3988635647dc7fd996
Al Noman <aalnomanm10@gmail.com>
2023-06-15 23:40:25 -0700

    get class by id

--- index.js
+++ index.js
@@ -101,6 +101,13 @@ async function run() {
             }
         })
 
+        app.get('/classes/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) };
+            const result = await classesCollection.findOne(query)
+            res.send(result)
+        })
+
         app.post('/classes', async (req, res) => {
             const newClass = req.body;
 
